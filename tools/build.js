1227bbc5a61b15ad8c87269f7bda590cec223600
jiangjie <jiangjie@intra.nsfocus.com>
2019-09-04 15:42:23 +0800

    init

---
+++ tools/build.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+const path = require('path');
+
+const DIST = 'dist';
+
+fs.removeSync(path.join(process.cwd(), DIST));
+
+fs.mkdirsSync(path.join(process.cwd(), DIST));
+
+fs.writeJSONSync(path.join(process.cwd(), DIST, 'test.json'), {name: 'wannaksy'});
+
